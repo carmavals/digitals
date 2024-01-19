89f94bc7a52ac044100065b2442dc63d29ef9680
carmavals <mvcarbajals@pucp.edu.pe>
2024-01-19 10:42:09 -0500

    nueva ed

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-let audio = document.getElementById("audio")
-let monologo = new Audio("assets/remember.mp3")
-let sonido = function(){
- monologo.play()
-}
+// let audio = document.getElementById("audio")
+// let monologo = new Audio("assets/remember.mp3")
+// let sonido = function(){
+//  monologo.play()
+// }
 
-audio.addEventListener("click", sonido)
+// audio.addEventListener("click", sonido)
 
+let form = document.querySelector('form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let respuesta = document.getElementById("recuerdo").value
+    let divResultado = document.createElement('div')
+   
+    divResultado.innerTEXT = `${respuesta}`
+let body = document.getElementById('body')
+body.appendChild(divResultado)
+    console.log(respuesta)
+
+})
